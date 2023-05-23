c76aa5c9980aa68b5102c86917da89c8f8a978a8
oblzh <openblocklzh@gmail.com>
2023-05-23 09:48:02 +0800

    add waitMined first and get Transaction, finished unittest

---
+++ metax/utils.go
@@ -0,0 +1,40 @@
+package metax
+
+import (
+	"context"
+	"errors"
+	"log"
+	"time"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+// WaitMined waits for tx to be mined on the blockchain.
+// It stops waiting when the context is canceled.
+func WaitMined(ctx context.Context, b bind.DeployBackend, txHash common.Hash) (*types.Receipt, error) {
+	queryTicker := time.NewTicker(time.Second)
+	defer queryTicker.Stop()
+
+	for {
+		receipt, err := b.TransactionReceipt(ctx, txHash)
+		if err == nil {
+			return receipt, nil
+		}
+
+		if errors.Is(err, ethereum.NotFound) {
+			log.Printf("Transaction %s not yet mined\n", txHash)
+		} else {
+			log.Printf("Receipt retrieval failed: %v\n", err)
+		}
+
+		// Wait for the next round.
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case <-queryTicker.C:
+		}
+	}
+}
